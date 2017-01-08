ee8e83e3bcb9ab576c3c5fe2ff9eeb2949fe860c
hexson <1477546184@qq.com>
2017-01-09 01:32:01 +0800

    [dev] update angular router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,61 @@
 (function(){
   'use strict';
 
-  angular.module('app', []);
+  angular.module('app', [
+    'ui.router',
+    'oc.lazyLoad',
+    'ngFileUpload',
+    'ngDialog',
+    'toastr',
+    'config'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$ocLazyLoadProvider', '$httpProvider', '$compileProvider', 'routes', function($stateProvider, $urlRouterProvider, $locationProvider, $ocLazyLoadProvider, $httpProvider, $compileProvider, routes){
+    console.log(routes);
+    $urlRouterProvider.otherwise('/main');
+    $stateProvider.state('main', {
+      url: '/main',
+      views: {
+        '': {
+          templateUrl: 'pages/main/main.html'
+        },
+        '@header': {
+          templateUrl: 'components/header/header.html'
+        },
+        '@footer': {
+          templateUrl: 'components/footer/footer.html'
+        }
+      },
+      resolve: {
+        loadMyFile: function($ocLazyLoad){
+          return $ocLazyLoad.load({
+            name: 'app',
+            files: [
+              'components/header/header.js',
+              'components/footer/footer.js'
+            ]
+          });
+        }
+      }
+    });
+    /* jshint ignore:start */
+    for (var route in routes){
+      $stateProvider.state('main.' + route, {
+        url: routes[route].url || ('/' + route),
+        templateUrl: routes[route].templateUrl || ('pages/' + route + '/' + route + '.html'),
+        resolve: {
+          loadMyFile: function($ocLazyLoad){
+            return $ocLazyLoad.load({
+              name: 'app',
+              files: routes[route].files || ['pages/' + route + '/' + route + '.js']
+            });
+          }
+        }
+      });
+      console.log(route, routes[route]);
+    }
+    /* jshint ignore:end */
+  }])
+  .run(['$rootScope', function($rootScope){
+    console.log($rootScope);
+  }]);
 })();
