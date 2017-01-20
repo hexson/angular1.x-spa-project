155fa8e382e895f3939c20937e1263149dd366fc
hexson <1477546184@qq.com>
2017-01-20 18:31:39 +0800

    [dev] update components

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,6 @@
 
   angular.module('app')
   .controller('header', ['$scope', '$state', function(vm, $state){
-    vm.header = 'header';
     vm.goIndex = function(){
       $state.go('main.index');
     }
