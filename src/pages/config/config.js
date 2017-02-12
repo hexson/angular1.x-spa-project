358c898d57dac35ef72c7cec5e865d2935e7ff8b
hexson <1477546184@qq.com>
2017-02-12 19:04:13 +0800

    [dev] update ctr js

--- src/pages/config/config.js
+++ src/pages/config/config.js
@@ -2,9 +2,7 @@
   'use strict';
 
   angular.module('app')
-  .controller('configctr', ['$scope', '$timeout', 'utils', function(vm, $timeout, _u){
-    $timeout(function(){
-      _u.hljs(angular.element(document).find('pre'));
-    }, 0);
+  .controller('configctr', ['utils', function(_u){
+    _u.hljs(angular.element(document).find('pre'));
   }]);
 })();
